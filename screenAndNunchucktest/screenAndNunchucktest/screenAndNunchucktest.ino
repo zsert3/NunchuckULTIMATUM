49efcd31c005c2358955e7abbb50f9c814fd11a5
Nieck Eikelenboom <s1078485@student.windesheim.nl>
2015-11-26 11:32:05 +0100

    basisfile

---
+++ screenAndNunchucktest/screenAndNunchucktest/screenAndNunchucktest.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		screenAndNunchucktest.ino
+ Created:	26-Nov-15 11:27:43 AM
+ Author:	Zsert
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
