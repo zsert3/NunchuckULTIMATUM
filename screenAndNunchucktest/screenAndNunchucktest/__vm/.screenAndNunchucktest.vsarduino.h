799c9d3f5b29e14c3662342bf3db8308c6bd7c08
Nieck Eikelenboom <s1078485@student.windesheim.nl>
2015-11-26 12:53:04 +0100

    test

--- screenAndNunchucktest/screenAndNunchucktest/__vm/.screenAndNunchucktest.vsarduino.h
+++ screenAndNunchucktest/screenAndNunchucktest/__vm/.screenAndNunchucktest.vsarduino.h
@@ -14,7 +14,7 @@
 #define __AVR_ATmega328P__
 #define _VMDEBUG 1
 #define F_CPU 16000000L
-#define ARDUINO 165
+#define ARDUINO 166
 #define ARDUINO_AVR_UNO
 #define ARDUINO_ARCH_AVR
 #define __cplusplus
