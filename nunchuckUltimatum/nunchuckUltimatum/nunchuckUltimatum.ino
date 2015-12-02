124e272699f88c297730da91d22fa54a9acb2c0d
Nieck Eikelenboom <s1078485@student.windesheim.nl>
2015-12-02 10:23:57 +0100

    dit worden de basisfiles voor ons project, ik moet nog wel kijken wat voor files ik ga toevoegen

---
+++ nunchuckUltimatum/nunchuckUltimatum/nunchuckUltimatum.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		nunchuckUltimatum.ino
+ Created:	02-Dec-15 10:22:10 AM
+ Author:	Zsert
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
