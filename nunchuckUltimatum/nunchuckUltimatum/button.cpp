86d9b3434d822115fc5348e32dc1232fbd582b45
bastiaan metz <bastiaanmetz@gmail.com>
2016-01-16 18:39:37 +0100

    ophalen laatste bestand

--- nunchuckUltimatum/nunchuckUltimatum/button.cpp
+++ nunchuckUltimatum/nunchuckUltimatum/button.cpp
@@ -4,14 +4,14 @@
 
 #include "button.h"
 //array size 
-int8_t arrSize = 0;
-int8_t widthButtons = 40;
-int8_t xValueButtons = 40;
-int8_t buttonLength = 160;
-int8_t button1X = 60;
-int8_t button2X = 125;
-int8_t button3X = 190;
-int8_t button4X = 255;
+uint8_t arrSize = 0;
+uint8_t widthButtons = 40;
+uint8_t xValueButtons = 40;
+uint8_t buttonLength = 160;
+uint8_t button1X = 60;
+uint8_t button2X = 125;
+uint8_t button3X = 190;
+uint8_t button4X = 255;
 
 void placeButton(MI0283QT9 lcd, int16_t backgroundColour, int16_t textColour, char *text, uint8_t lcX, uint16_t lcY, uint8_t buttonSizeY, uint8_t buttonSizeX) {
 	//add text if there is any text
@@ -22,37 +22,37 @@ void placeButton(MI0283QT9 lcd, int16_t backgroundColour, int16_t textColour, ch
 	lcd.drawText(lcX + (buttonSizeX / 2) - (arrSize * 4), lcY + (buttonSizeY / 2) - 4, text, textColour, backgroundColour, 1);
 }
 
-int8_t getWidthButtons()
+uint8_t getWidthButtons()
 {
 	return widthButtons;
 }
 
-int8_t getXValueButtons()
+uint8_t getXValueButtons()
 {
 	return xValueButtons;
 }
 
-int8_t getButtonLength()
+uint8_t getButtonLength()
 {
 	return buttonLength;
 }
 
-int8_t getButton1X()
+uint8_t getButton1X()
 {
 	return button1X;
 }
 
-int8_t getButton2X()
+uint8_t getButton2X()
 {
 	return button2X;
 }
 
-int8_t getButton3X()
+uint8_t getButton3X()
 {
 	return button3X;
 }
 
-int8_t getButton4X()
+uint8_t getButton4X()
 {
 	return button4X;
 }
